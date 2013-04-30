e2bf5066071a6430f4837ff1cdb4d9417e2f54f3
Sean Mallard <pattojones@gmail.com>
2013-04-30 22:45:48 +1000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PrototypeOne::Application.config.secret_token = '9de46bad7b689eb7c96dad49f86dce7e9fffa3c9c5a3392a1a6c789d2218fb6d9453f49f4fc43bb7b2a69368b2314df7ecfb3abe3cdd87b2a4e543880284369c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+PrototypeOne::Application.config.session_store :cookie_store, key: '_prototype_one_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# PrototypeOne::Application.config.session_store :active_record_store
